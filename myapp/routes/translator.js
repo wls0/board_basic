35586b7dc5a82d79abc2da76eb8fd5ff9774caf3
Crispyfrenchfries <kjy0836@naver.com>
2020-03-04 23:51:55 +0900

    번역기 ajax 시도중

--- myapp/routes/translator.js
+++ myapp/routes/translator.js
@@ -1,34 +1,33 @@
 var express = require('express');
 var router = express.Router();
-var client_id = 'hlOJ1di6yot7rckQmOj8';
-var client_secret = 'hQGUWpA3Fg';
+var secret = require('../db/secret');
 var request = require('request');
 
 router.get('/', function (req, res, next) {
   let session = req.session.passport;
+  console.log(req.query.translator);
+  let result = req.query.translator;
   res.render('translator', {
-    session: session
+    session: session,
+    result: result
   });
 });
 
 router.get('/result', function (req, res) {
   let session = req.session.passport;
-  var query = req.query.translator;
+  var query = req.query;
+  console.log(query);
   var api_url = 'https://openapi.naver.com/v1/papago/n2mt';
   var options = {
     url: api_url,
     form: { 'source': 'ko', 'target': 'en', 'text': query },
-    headers: { 'X-Naver-Client-Id': client_id, 'X-Naver-Client-Secret': client_secret }
+    headers: { 'X-Naver-Client-Id': secret.naver.clientID, 'X-Naver-Client-Secret': secret.naver.clientSecret }
   };
   request.post(options, function (error, response, body) {
     if (!error && response.statusCode === 200) {
-      // res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
       let json = JSON.parse(body);
       let result = json.message.result.translatedText;
-      console.log(typeof (result));
-      // res.end(result);
-      // res.redirect('/translator');
-      res.render('translator_result', {
+      res.render('translator', {
         session: session,
         result: result,
         query: query
